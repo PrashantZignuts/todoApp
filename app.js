900ccb5ecb510503e90c154e4dbb9f39ebf43940
PrashantZignuts <prashantn@zignuts>
2022-12-02 17:10:29 +0530

    connected to database and created schema and model

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-const express = require("express")
-const app = express()
-const todoController = require("./controllers/todoController")
+const express = require("express");
+const app = express();
+const todoController = require("./controllers/todoController");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//connect to database
+  mongoose 
+ .connect(process.env.MONGODB_URL)   
+ .then(() => console.log("Database connected!"))
+ .catch(err => console.log(err));
+
+
+//create schema
+var todoSchema = new mongoose.Schema({
+  item: String,
+});
+
+// create model
+var Todo = mongoose.model("Todo", todoSchema);
+
+//create a fake datas
+var itemOne = Todo({item: 'buy flowers'}).save((err) => {
+    if(err) throw err
+    console.log("Data is saved");
+})
 
 //setting view engine
-app.set("view engine","ejs")
+app.set("view engine", "ejs");
 
 // setting static file
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 
 //loading controllers
-todoController(app)
+todoController(app);
 
-//serving port 
-app.listen(8080,() => {
-    console.log('Serving on Port 8080');
-})
+//serving port
+app.listen(8080, () => {
+  console.log("Serving on Port 8080");
+});
