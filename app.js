75d3177758d4616db35aeb9518230cca2f3d6e12
PrashantZignuts <prashantn@zignuts>
2022-12-02 15:53:37 +0530

    initialize todo app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const todoController = require("./controllers/todoController")
+
+//setting view engine
+app.set("view engine","ejs")
+
+// setting static file
+app.use(express.static('./public'))
+
+//loading controllers
+todoController(app)
+
+//serving port 
+app.listen(8080,() => {
+    console.log('Serving on Port 8080');
+})
